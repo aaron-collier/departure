f48ac5c8dd20a83d0efa9096a290ecad94d66825
Aaron Collier <acollier@calstate.edu>
2017-02-25 10:25:52 -0800

    Pass id parameter to community presenter

--- app/controllers/dspace/community_controller.rb
+++ app/controllers/dspace/community_controller.rb
@@ -9,7 +9,7 @@ class Dspace::CommunityController < ApplicationController
 
   def show
     @community = Dspace::Community.find(params[:id])
-    @comm_info = Dspace::CommunityPresenter.new
+    @comm_info = Dspace::CommunityPresenter.new(params[:id])
     # @items = Dspace::Item.where(item_id: Dspace::CommunitiesToItem.where(community_id: params[:id]).item_id).find_each
     # @items = Dspace::Item.join(Dspace::CommunitiesToItem: .where(community_id: params[:id]).item_id).find_each
   end
